26766173f887dfd22a30215927a2cceb4ccc2340
monkdaf <monkdaf@gmail.com>
2017-07-28 17:19:24 +0300

    add simple VS aplication for run storage procedure

---
+++ VisualStudio/runStoredProcedure/runStoredProcedure/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+
+namespace runStoredProcedure
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var sqlConn = new SqlConnection("Server=ASU-PC\\WINCC;Database=testVS;Trusted_Connection=True;"))
+            {
+                var sqlCmd = new SqlCommand("procVS", sqlConn);
+                sqlCmd.CommandType = CommandType.StoredProcedure;
+
+                sqlConn.Open();
+                sqlCmd.ExecuteNonQuery();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            using (var sqlConn = new SqlConnection("Server=ASU-PC\\WINCC;Database=testVS;Trusted_Connection=True;"))
+            {
+                var sqlCmd = new SqlCommand("procVS", sqlConn);
+                sqlCmd.CommandType = CommandType.StoredProcedure;
+
+                sqlConn.Open();
+                sqlCmd.ExecuteNonQuery();
+            }
+
+        }
+    }
+}
